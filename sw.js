d00bb689570d996f6175f8b18efc8c1bb7ddf125
adangelo1996-eng <a.dangelo1996@gmail.com>
2026-02-18 07:11:17 +0100

    Update service worker for cache management

--- sw.js
+++ sw.js
@@ -1,16 +1,29 @@
-const CACHE_NAME = 'parfum-studio-v1';
+const CACHE = 'parfum-studio-v2';
 const ASSETS = [
-  './',
   './index.html',
-  'https://fonts.googleapis.com/css2?family=Courier+Prime:wght@400;700&display=swap'
+  './manifest.json',
+  './icon-192.svg',
+  './icon-512.svg'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
